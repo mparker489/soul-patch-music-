5f715e8f59aded1700dce4b2f7e03a39984a3861
parkerme <meghan.parker@cdk.com>
2017-07-10 16:50:30 -0700

    crying a little less

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/static', express.static('public'));
+
 app.set('view engine', 'pug');
 
+//routes
 app.get('/', (req, res) => {
     res.render('home-page');
 });
 
-app.get('/all-albums', (req, res) => {
-    res.send("Welcome to all albums!");
+app.get('/genres', (req, res) => {
+    res.render('genre-page');
 });
 
 app.listen(1234, () => {
