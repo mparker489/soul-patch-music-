cc55d7c90777512440111b40cdf57ac6809b9ba1
Julia Marshall <julia.marshall@cdk.com>
2017-07-10 15:59:02 -0700

    oops

--- routes/genre.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { data } = require(../data/allAlbums.json);
-const { albums } = data; 
-
-router.get('/', (req, res) => {
-    const 
-});
