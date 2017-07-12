63f02c60b5c35d1ff4e3157ec95b776d548b456a
traceyh <halina.tracey@cdk.com>
2017-07-12 16:28:21 -0700

    I hope i didn't break this

--- public/javascript/searchbar.js
+++ public/javascript/searchbar.js
@@ -3,7 +3,7 @@ function myFunction(){
   input= document.getElementById('myInput');
   filter= input.value.toUpperCase();
   ul = document.getElementById("myUL");
-  li = ul.getElementsByTagName('li');
+  li = ul.getElementsByClassName('textGenre');
 
   for( i = 0; i < li.length; i++)
     {
