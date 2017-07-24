cb6f0b3c185ecd1257fb1b1fc7571bf4593648e2
Julia Marshall <julia.marshall@cdk.com>
2017-07-24 12:06:13 -0700

    fix error page

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ app.use((req, res, next) => {
     next(err);
 });
 
-// app.use((err, req, res, next) => {
-//     res.locals.error = err;
-//     res.status(err.status);
-//     res.render('error');
-// });
+app.use((err, req, res, next) => {
+    res.locals.error = err;
+    res.status(err.status);
+    res.render('error');
+});
 
 
 app.listen(1234, () => {
