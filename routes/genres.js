1b6ffe826b18549c418299fe082f1e72604a459f
Julia Marshall <julia.marshall@cdk.com>
2017-07-24 17:04:39 -0700

    this was going well and then it didnt

--- routes/genres.js
+++ routes/genres.js
@@ -2,11 +2,16 @@ const express = require('express');
 const router = express.Router();
 const {data} = require("../data/albums.json");
 const {pop} = data;
+const{rap} = data;
 
-console.log(pop[2].description);
+var genre = [];
+for(var i = 0; i < data.length; i++){
+    genre.push(data[i]._id)
+}
 
 router.get('/', (req, res) => {
-    res.render('genres', data);
+    res.render('genres', {rap});
+
 });
 
 module.exports = router;
