cdb9e23d5d8f964c04dd4c1061f2b560b3dc1c7b
traceyh <halina.tracey@cdk.com>
2017-07-26 10:01:24 -0700

    changing the json

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const data = require("../data/albums.json");
 
 router.get('/', (req, res) => {
-    res.render('home-page');
+  var all = [];
+  Object.keys(data.data).forEach(function(key){
+      all = all.concat(data.data[key].albums);
+  })
+    res.render('home-page', {data:all});
 });
 
 module.exports = router;
