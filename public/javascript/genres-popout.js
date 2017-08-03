6a42d58765da0e81b02a361530b24ef7aa455ff1
parkerme <meghan.parker@cdk.com>
2017-08-03 13:39:10 -0700

    taking out the snazy little console log thing

--- public/javascript/genres-popout.js
+++ public/javascript/genres-popout.js
@@ -4,10 +4,8 @@ document.getElementById("close-button").addEventListener("click", closeNav);
 function openNav(evt){
     evt.preventDefault();
     document.querySelector(".pop-out").classList.remove("close");
-    console.log("yay");
 }
 function closeNav(evt){
     evt.preventDefault();
     document.querySelector(".pop-out").classList.add("close");
-    console.log("yay");
 }
