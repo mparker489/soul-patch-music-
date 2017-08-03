0fc671e67c2e30cb25ef09f598a610f1c649dcbe
Julia Marshall <julia.marshall@cdk.com>
2017-08-03 14:39:56 -0700

    merge

---
+++ public/javascript/top-button.js
@@ -0,0 +1,5 @@
+var offset = 300,
+	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
+	offset_opacity = 1200,
+	//duration of the top scrolling animation (in ms)
+    scroll_top_duration = 700;
