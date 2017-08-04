84e276c0c3730e8a90dd3ffbb3b94ea044e9c7e6
Julia Marshall <julia.marshall@cdk.com>
2017-08-04 09:39:54 -0700

    top button is back bby

--- public/javascript/top-button.js
+++ public/javascript/top-button.js
@@ -1,5 +1,14 @@
-var offset = 300,
-	//browser window scroll (in pixels) after which the "back to top" link opacity is reduced
-	offset_opacity = 1200,
-	//duration of the top scrolling animation (in ms)
-    scroll_top_duration = 700;
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+}
